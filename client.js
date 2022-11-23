75c536b09fd2339aebe66347c83d1cd444f0fc99
Steve Willsmore <scwillsmore@gmail.com>
2022-11-23 18:19:17 -0500

    Moved IP and PORT to constants.js.

--- client.js
+++ client.js
@@ -1,9 +1,10 @@
 // establishes a connection with the game server
 const net = require("net");
+const { IP, PORT } = require("./constants");
 const connect = function () {
   const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: 50541
+    host: IP,
+    port: PORT
   });
 
   // interpret incoming data as text
