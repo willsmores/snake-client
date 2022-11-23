6e4c7bb49dc46b0c95369a6345a18fb58dde047a
Steve Willsmore <scwillsmore@gmail.com>
2022-11-23 14:44:20 -0500

    Refactored setup and userinput functions to input.js.

---
+++ input.js
@@ -0,0 +1,21 @@
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+const handleUserInput = function () {
+  const stdin = process.stdin;
+  stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  });
+}
+
+module.exports = setupInput;
