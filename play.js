2af4a5caa3745da371872bb0e7b762130787ad0b
Steve Willsmore <scwillsmore@gmail.com>
2022-11-23 13:48:12 -0500

    Separated connect function.

--- play.js
+++ play.js
@@ -1,21 +1,5 @@
 const net = require("net");
-
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: 50541
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("data", (data) => {
-    console.log("Server says: ", data);
-  });
-
-  return conn;
-};
+const connect = require("./client");
 
 console.log("Connecting ...");
 connect();
